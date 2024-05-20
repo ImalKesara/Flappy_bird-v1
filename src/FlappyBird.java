a837fcac0ee0cc12c7035c1a7ee396df5349395b
LoneWolf <imalkesara2001@gmail.com>
2024-05-21 01:01:41 +0530

    setting canva

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -1,2 +1,15 @@
-public class FlappyBird {
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random; //random places for pipes
+import java.util.ArrayList; // for save pipes
+
+public class FlappyBird extends JPanel{
+    int BoardWidth = 360;
+    int BoardHeight = 640;
+
+    FlappyBird(){
+        setPreferredSize(new Dimension(BoardWidth,BoardHeight));
+        setBackground(Color.blue);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,16 @@ public class Main {
     public static void main(String[] args) {
         int BoardWidth = 360;
         int BoardHeight = 640;
+
+        FlappyBird flappyBird = new FlappyBird();
         JFrame frame =  new JFrame("Flappy bird");
         frame.setSize(BoardWidth,BoardHeight);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(flappyBird);
+        frame.pack();
 
 
     }
