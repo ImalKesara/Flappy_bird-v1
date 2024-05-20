93979d6e6afca56d283209932e8a2a4b466baafb
LoneWolf <imalkesara2001@gmail.com>
2024-05-21 00:51:19 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int BoardWidth = 360;
+        int BoardHeight = 640;
+        JFrame frame =  new JFrame("Flappy bird");
+        frame.setSize(BoardWidth,BoardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+    }
+}
